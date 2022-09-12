24e1493caa5997d82350d441f19acd7e3b9ef009
Anushal14 <anushal0483@gmail.com>
2022-09-12 14:03:58 +0530

    created a update and delete routes

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const placeRoutes = require('./routes/places-routes')
+const placeRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app =express();
 
 app.use(bodyParser.json())
 
 app.use('/api/places',placeRoutes)
+
+app.use((req,res,next)=>{
+    const error = new HttpError("Couldn't find this route",404)
+    throw error;
+})
 
 app.use((error,req,res,next)=>{
     if(res.headerSent){
--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,8 +1,8 @@
-const uuid = require('uuid')
+const {v4:uuidv4} = require('uuid')
 const HttpError = require('../models/http-error')
 
 
-const DUMMY_PLACES =[
+let DUMMY_PLACES =[
     {
         id:'p1',
         title:"Infopark Kochi",
@@ -42,7 +42,7 @@ const getPlaceByUserId = (req,res,next)=>{
 const createPlace = (req,res,next) => {
     const {title,description,coordinates,address,creator} = req.body;
     const createdPlace = {
-        // id:uuid(),
+        id:uuidv4(),
         title,
         description,
         location:coordinates,
@@ -53,6 +53,26 @@ const createPlace = (req,res,next) => {
     res.status(201).json({place:createdPlace})
 }
 
+const updatePlace =(req,res,next) => {
+    const {title,description} = req.body;
+    const placeId = req.params.pid; 
+    const updatedPlace ={...DUMMY_PLACES.find(p=>p.id === placeId)};
+    const placeIndex = DUMMY_PLACES.findIndex(p=>p.id ===placeId)
+    updatedPlace.title = title;
+    updatedPlace.description = description;
+    DUMMY_PLACES[placeIndex] = updatedPlace;
+    res.status(200).json({place:updatedPlace});
+}
+
+const deletePlace =(req,res,next) => {
+    const placeId = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(p=>p.id !== placeId);
+    res.status(200).json({message:"Deleted place"})
+
+}
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
