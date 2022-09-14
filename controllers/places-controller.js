97771ac4468d8377cbe4782cdf9e3eb39bb8d2d6
Anushal14 <anushal0483@gmail.com>
2022-09-15 00:28:08 +0530

    deleting places and remove it from user

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -131,14 +131,22 @@ const deletePlace = async (req, res, next) => {
     const placeId = req.params.pid;
     let place;
     try{
-        place = await Place.findById(placeId);
+        place = await Place.findById(placeId).populate('creator');
     }catch(err){
         const error = new HttpError(err,500);
         return next(error)
     }
-
+    if(!place){
+        const error = new HttpError("could not find the place for provided id",404)
+        return next(error);
+    }
     try{
-        await place.remove()
+        const sess = await mongoose.startSession();
+        sess.startTransaction()
+        await place.remove({session: sess})
+        place.creator.places.pull(place)
+        await place.creator.save({session : sess});
+        await sess.commitTransaction();
     }catch(err){
         const error = new HttpError(err,500);
         return next(error)
